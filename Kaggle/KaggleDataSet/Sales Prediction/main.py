18cca43fd4ae9203debd1f2e6a45bafa0ecd4b56
ZefanLin159 <915654746@qq.com>
2020-12-17 00:17:23 +0800

    Kaggle

---
+++ Kaggle/KaggleDataSet/Sales Prediction/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+train_set = pd.read_csv(
+    'F:/work/Kaggle/KaggleDataSet/competitions/competitive-data-science-predict-future-sales/sales_train.csv')
+
+train_set.info()
