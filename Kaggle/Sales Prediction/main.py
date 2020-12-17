f0b1fe4a25944901f33a27f8cccaf0aa4097f5ad
LinZefan159 <915654746@qq.com>
2020-12-17 21:01:30 +0800

    -a

--- Kaggle/Sales Prediction/main.py
+++ Kaggle/Sales Prediction/main.py
@@ -4,4 +4,5 @@ from loadDataSet import pd, train
 train['year'] = pd.to_datetime(train['date']).dt.strftime('%Y')
 train['month'] = train.date.apply(lambda x: datetime.strptime(x, '%d.%m.%Y').strftime('%m'))
 
-test = train.head(2)
+pd.set_option('display.max_columns', 8)
+print(train.head(2))
