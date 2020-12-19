4169b0d4e77195c1536d44341ed4116249362aae
LinZefan159 <915654746@qq.com>
2020-12-19 15:49:18 +0800

    -a

---
+++ Kaggle/Sales Prediction/totalSale.py
@@ -0,0 +1,21 @@
+import numpy as np
+import seaborn as sns
+from loadDataSet import pd, train, plt
+
+ts = train.groupby(["date_block_num"])["item_cnt_day"].sum()
+ts.astype('float')
+plt.figure(figsize=(16, 8))
+plt.title('Total Sales of the whole time period')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+plt.plot(ts)
+# plt.show()
+print('testing TotalSale')
+
+train.item_cnt_day.hist(bins=100)
+# print(train.item_cnt_day.describe())
+# plt.show()
+
+print('Data set size before remove item price 0 cleaning:', train.shape)
+train = train.query('item_price > 0')
+print('Data set size after remove item price 0 cleaning:', train.shape)
