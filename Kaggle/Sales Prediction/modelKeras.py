c64f930efcaddeeb772b0511122daaa82b9dc0d3
LinZefan159 <915654746@qq.com>
2020-12-22 11:23:58 +0800

    -a

--- Kaggle/Sales Prediction/modelKeras.py
+++ Kaggle/Sales Prediction/modelKeras.py
@@ -1,3 +1,15 @@
-from keras.models import Sequential
-from keras.layers import LSTM,Dense,Dropout
-from keras.models import load_model, Model
+
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model, Model
+
+
+
+# our defining sales model
+sales_model = Sequential()
+sales_model.add(LSTM(units=64, input_shape=(33, 1)))
+sales_model.add(Dropout(0.5))
+sales_model.add(Dense(1))
+
+sales_model.compile(loss='mse', optimizer='adam', metrics=['mean_squared_error'])
+sales_model.summary()
