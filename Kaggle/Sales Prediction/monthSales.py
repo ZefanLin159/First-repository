de4a12f875800e21966a86e92978d330b5bdc435
LinZefan159 <915654746@qq.com>
2021-01-01 10:49:58 +0800

    -a

--- Kaggle/Sales Prediction/monthSales.py
+++ Kaggle/Sales Prediction/monthSales.py
@@ -8,20 +8,22 @@ monthly_sales_train = train.groupby(["date_block_num", "shop_id", "item_id"])[
     "date_block_num", "date", "item_price", "item_cnt_day"].agg(
     {"date_block_num": 'mean', "date": ["min", 'max'], "item_price": "mean", "item_cnt_day": "sum"})
 
-print(monthly_sales_train.head(5))
+print(monthly_sales_train.head(3))
 
 sales_data_flat = monthly_sales_train.item_cnt_day.apply(list).reset_index()
 # Keep only the test data of valid
+
+print(sales_data_flat.head(3))
 sales_data_flat = pd.merge(test, sales_data_flat, on=['item_id', 'shop_id'], how='left')
 # fill na with 0
 sales_data_flat.fillna(0, inplace=True)
 sales_data_flat.drop(['shop_id', 'item_id'], inplace=True, axis=1)
-print(sales_data_flat.head(10))
+print(sales_data_flat.head(3))
 
 
-# We will create pivot table.
-# Rows = each shop+item code
-# Columns will be out time sequence
+# 创建数据透视表.
+# 行=每个商店+物品代码
+# 列将按时间顺序输出
 pivoted_sales = sales_data_flat.pivot_table(index='ID', columns='date_block_num', fill_value=0, aggfunc='sum')
 print(pivoted_sales.head(10))
 
