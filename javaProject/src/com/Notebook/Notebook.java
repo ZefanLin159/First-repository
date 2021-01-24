da92a158bf95cfae252fee6a21c724b8e24c2928
ZefanLin159 <915654746@qq.com>
2021-01-24 10:32:40 +0800

    -a

---
+++ javaProject/src/com/Notebook/Notebook.java
@@ -0,0 +1,24 @@
+package com.Notebook;
+
+public class Notebook {
+    //容器
+    public void add(String s){
+
+    }
+    public int getSize(){
+        return 0;
+    }
+    public String getNote(int index){
+         return "";
+
+    }
+    public boolean removeNot(int index){
+        return true;
+    }
+    public String[] list(){
+        return new String[10];
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- javaProject/src/com/display/Clock.java
+++ javaProject/src/com/display/Clock.java
@@ -5,6 +5,7 @@ public class Clock {
     private Display minutes = new Display(60);
 
     public void start() {
+
         while (true) {
             minutes.increase();
             if (minutes.getValue() == 0) {
@@ -14,8 +15,20 @@ public class Clock {
         }
     }
 
+    //static 类变量（不属于任何具体对象），可以通过 类名.变量 或 对象.变量 来访问。
+    // static+函数名 类函数
+    //static 中不能访问non-static域
     public static void main(String args[]) {
         Clock clock = new Clock();
         clock.start();
+
+
     }
 }
+
+//对象是鸡蛋，对象中的数据是蛋黄，对象中的操作是蛋白，包装成鸡蛋的过程就是封装
+//对象=属性+服务
+//private 只能用于成员变量或成员函数（在类的外面不可访问了）
+//只有这个类内部可以访问：1.类内部指类的成员函数和定义初始化2.这个限制对象是针对类的而不是函数。
+//如果没有public private,那么成为friendly，同一个包的可以访问
+//public 都可以访问
