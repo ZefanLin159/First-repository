23576181d34fb6c0983543f78536efb85f8e437e
ZefanLin159 <915654746@qq.com>
2021-03-01 22:48:07 +0800

    0a

---
+++ leetcode/easy/src/com/Array/TwoSum01.java
@@ -0,0 +1,14 @@
+package com.Array;
+
+public class TwoSum01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution01{
+    public int[] twoSum(int[] nums, int target) {
+        int num;
+        return nums;
+    }
+}
