313ec211fda35af56a7dd7894989492b72a12200
ZefanLin159 <915654746@qq.com>
2021-03-01 17:40:28 +0800

    -a

---
+++ leetcode/easy/src/com/LinkedList/IntersectionOfTwoLinkedLists160.java
@@ -0,0 +1,22 @@
+package com.LinkedList;
+
+public class IntersectionOfTwoLinkedLists160 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
