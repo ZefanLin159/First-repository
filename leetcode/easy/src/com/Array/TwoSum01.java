1d725595324a27ad872811c6d5c759a7e5249940
ZefanLin159 <915654746@qq.com>
2021-03-02 08:46:33 +0800

    -a

--- leetcode/easy/src/com/Array/TwoSum01.java
+++ leetcode/easy/src/com/Array/TwoSum01.java
@@ -6,9 +6,19 @@ public class TwoSum01 {
     }
 }
 
-class Solution01{
+//时间复杂度O(N),空间复杂度O(1);
+class Solution01 {
     public int[] twoSum(int[] nums, int target) {
-        int num;
-        return nums;
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
     }
 }
