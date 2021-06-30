60b4d51c0fb48c2ebc079817f37b68e70282a2d2
ZefanLin159 <915654746@qq.com>
2021-06-30 22:38:30 +0800

    -a

--- javaSE/src/com/MyInternet/InternetAddDemo.java
+++ javaSE/src/com/MyInternet/InternetAddDemo.java
@@ -1,2 +1,14 @@
-package com.MyInternet;public class InternetAddDemo {
+package com.MyInternet;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InternetAddDemo {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = InetAddress.getByName("192.168.184.1");
+        String hostName = address.getHostName();
+        String ip = address.getHostAddress();
+        System.out.println(hostName);
+        System.out.println(ip);
+    }
 }
