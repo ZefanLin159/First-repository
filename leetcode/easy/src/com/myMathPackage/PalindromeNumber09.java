a2cacb28a81d609f9dfd654c8504c367c4718a24
LinZefan159 <915654746@qq.com>
2021-07-05 14:56:06 +0800

    -updatePractice06And07

--- leetcode/easy/src/com/myMathPackage/PalindromeNumber09.java
+++ leetcode/easy/src/com/myMathPackage/PalindromeNumber09.java
@@ -14,7 +14,7 @@ public class PalindromeNumber09 {
             x = x / 10;
             arrayList.add(i);
         } while (x > 0);
-        for (i = 0, j = arrayList.size()-1; i <= j; i++, j--) {
+        for (i = 0, j = arrayList.size() - 1; i <= j; i++, j--) {
             if (arrayList.get(i) != arrayList.get(j)) {
                 flag = false;
                 break;
@@ -32,7 +32,56 @@ public class PalindromeNumber09 {
 //        System.out.println(isPalindrome(121));
         int test = 1221;
         //精彩
-        int count = (int)(1+Math.log(test)/Math.log(10));
+        int count = (int) (1 + Math.log(test) / Math.log(10));
         System.out.println(count);
+    }
+    //需要额外空间，效果不好，不是最优解
+    public static boolean isPalindrome1(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        boolean isPal = false;
+        StringBuilder s = new StringBuilder();
+        int temp;
+        while (x>0){
+            temp = x % 10;
+            s.append(temp);
+            x/=10;
+        }
+        String newS = s.toString();
+        int i = 0,j = newS.length()-1;
+        while (i<j){
+            if(newS.charAt(i) == newS.charAt(j)){
+                i++;
+                j--;
+                isPal = true;
+            }else {
+                isPal = false;
+                break;
+            }
+        }
+        return isPal;
+    }
+    //利用第七题的解法
+    public static boolean isPalindrome2(int x){
+        if (x < 0) return false;
+        boolean isPal = false;
+
+        int i = 0;
+        long y = x;
+        int newX = x;
+        while(y>0){
+            y/=10;
+            i++;
+        }
+        int lowBit = 0;
+        while(newX>0){
+            lowBit = newX%10;
+            newX = newX/10;
+            y += lowBit*(long)Math.pow(10,i-1);
+            i--;
+        }
+        if(y>=Integer.MAX_VALUE) return false;
+        if(x == y) isPal = true;
+        return isPal;
     }
 }
--- leetcode/easy/src/com/myMathPackage/ReverseInteger07.java
+++ leetcode/easy/src/com/myMathPackage/ReverseInteger07.java
@@ -8,7 +8,7 @@ public class ReverseInteger07 {
         long reverseX = 0;
         if(x!=0) {
             do {
-                reverseX = reverseX * 10 + x % 10;
+                reverseX = reverseX * 10 + x % 10;//每次将反转数据乘10就可以了
                 x /= 10;
             } while (x != 0);
         }
@@ -16,7 +16,33 @@ public class ReverseInteger07 {
     }
 
     public static void main(String[] args) {
-
-
+        int x = 123;
+        int i = reverse2(x);
+        System.out.println(i);
+    }
+    //自己想的比最优解多1ms，因为计算了长度（是否有更好的方法？），不过没注意反转后数据范围的问题，这里一定要注意！要用long去定义翻转后的数据，返回时再增加判断语句。
+    public static int reverse2(int x) {
+        boolean isNegative = false;
+        if(x<0){
+            isNegative = true;
+            x = -x;
+        }
+        int i = 0;
+        long y = x;
+        while(y>0){
+            y/=10;
+            i++;
+        }
+        int lowBit = 0;
+        while(x>0){
+            lowBit = x%10;
+            x = x/10;
+            y += lowBit*(long)Math.pow(10,i-1);
+            i--;
+        }
+        if(isNegative == true){
+            y = -y;
+        }
+        return y>Integer.MAX_VALUE || y<Integer.MIN_VALUE? 0:(int)y;
     }
 }
