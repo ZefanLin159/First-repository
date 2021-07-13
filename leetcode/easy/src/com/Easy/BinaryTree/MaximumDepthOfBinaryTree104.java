2f7990178372ae134314b0542484a30caa61ce0c
ZefanLin159 <915654746@qq.com>
2021-07-13 17:09:07 +0800

    -updateLeetcode104

--- leetcode/easy/src/com/Easy/BinaryTree/MaximumDepthOfBinaryTree104.java
+++ leetcode/easy/src/com/Easy/BinaryTree/MaximumDepthOfBinaryTree104.java
@@ -1,4 +1,14 @@
 package com.Easy.BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaximumDepthOfBinaryTree104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        //如果不加1的话会出现输出全为0的情况，就是要加上根节点的深度
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+   
 }
