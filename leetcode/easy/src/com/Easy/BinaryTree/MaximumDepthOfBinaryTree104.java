9c0a9eb9f1e27b6924cd419a312e9b26a12fb70f
ZefanLin159 <915654746@qq.com>
2021-07-15 11:52:22 +0800

    -updateLeetcode104

--- leetcode/easy/src/com/Easy/BinaryTree/MaximumDepthOfBinaryTree104.java
+++ leetcode/easy/src/com/Easy/BinaryTree/MaximumDepthOfBinaryTree104.java
@@ -1,14 +1,32 @@
 package com.Easy.BinaryTree;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class MaximumDepthOfBinaryTree104 {
+    //递归，DFS
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
         //如果不加1的话会出现输出全为0的情况，就是要加上根节点的深度
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
+    //层序遍历，BFS
+    public int maxDepth_1(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode temp = new TreeNode();
+        queue.offer(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            while (count-- > 0) {
+                temp = queue.poll();
+                if (temp.left != null) queue.offer(temp.left);
+                if (temp.right != null) queue.offer(temp.right);
+            }
+            level++;
+        }
+        return level;
+    }
 
 }
