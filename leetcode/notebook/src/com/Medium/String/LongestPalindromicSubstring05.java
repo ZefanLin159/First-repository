2a3e9130c30225798c3629d42efe31529eea193d
ZefanLin159 <915654746@qq.com>
2021-09-06 09:44:43 +0800

    leetcodeUncomplete

---
+++ leetcode/notebook/src/com/Medium/String/LongestPalindromicSubstring05.java
@@ -0,0 +1,10 @@
+package com.Medium.String;
+
+public class LongestPalindromicSubstring05 {
+    public String longestPalindrome(String s) {
+        if(s.length() == 0 || s.length() == 1) return s;
+        StringBuilder sb = new StringBuilder();
+        int i = 0,j = s.length()-1;
+        return s;
+    }
+}
